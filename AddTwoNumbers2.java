ec9fb9ba32f1a3a5a0aa5d17a7e0f12ca48481a0
Naveenkumarm01 <143997548+Naveenkumarm01@users.noreply.github.com>
2023-11-29 14:06:28 +0530

    Update AddTwoNumbers2.java

--- AddTwoNumbers2.java
+++ AddTwoNumbers2.java
@@ -1,11 +1,8 @@
-import java.util.Scanner;
 public class AddTwoNumbers2 {
-
-    public class SumOfNumbers1  
-{  
 public static void main(String args[])   
 {  
-int n1 = 225, n2 = 115, sum;  
+int n1 = 225;
+int n2 = 115;  
 sum = n1 + n2;  
 System.out.println("The sum of numbers is: "+sum);  
 }  
